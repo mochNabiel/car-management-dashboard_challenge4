24145b77ce7d9aead2c7c345fa04f9ee60a6051b
Mochammad nabiel <mochnabiel77@gmail.com>
2024-03-30 22:12:07 +0700

    Menambahkan ejs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const router = require("./routes");
@@ -5,6 +6,11 @@ const router = require("./routes");
 const app = express();
 const port = 3000;
 
+// Set view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware
 app.use(express.json());
 app.use(fileUpload({ useTempFiles: true }));
 app.use(express.urlencoded({ extended: true }));
@@ -12,12 +18,25 @@ app.use(express.static("public"));
 
 app.use("/api", router);
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/", (req, res) => {
+  res.render("addCarPage");
+});
+
+app.get("/update-car", (req, res) => {
+  res.render("updateCarPage");
+});
+
 app.use("*", (req, res) => {
   res.status(404).json({
     data: null,
     message: "Route not found",
   });
 });
+
 
 app.use((err, req, res, next) => {
   let statusCode = 500;
